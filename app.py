9b02e526a939c62cfb91e7ed56705b38058c491c
rsaund21 <rsaund21@umd.edu>
2024-03-26 21:05:23 -0400

    Added necessary app.route extensions and coded corresponding html pages to satisfy stated user input business requirements.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -15,6 +15,24 @@ def about():
 @app.route('/about-css')
 def about_css():
     return render_template('about-css.html')
+
+@app.route('/favorite-course', methods=['GET', 'POST'])
+def favorite_course():
+    print(f'Subject: {request.args.get("class_name")}')
+    print(f'Course Number: {request.args.get("course_number")}')
+
+    return render_template('favorite-course.html')
+
+@app.route('/contact', methods=['GET', 'POST'])
+def contact():
+    if request.method == 'POST':
+        return render_template('contact.html', form_submitted=True)
+    else:
+        return render_template('contact.html')
+
+@app.route('/base')
+def base():
+    return render_template('base.html')
 
 
 if __name__ == '__main__':
